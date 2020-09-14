d9ff81f0671835e26a150af0e4c7e1dfb4a55078
Win-Man <825895587@qq.com>
2020-09-14 20:33:33 +0800

    add tidb data

--- command/test.go
+++ command/test.go
@@ -1,7 +1,10 @@
 package command
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/spf13/cobra"
+	"time"
 )
 
 func newTestCmd() *cobra.Command {
@@ -16,6 +19,8 @@ func newTestCmd() *cobra.Command {
 			switch args[0] {
 			case "log":
 				logTest()
+			case "tidb":
+				tidbTest()
 			default:
 				return cmd.Help()
 			}
@@ -25,6 +30,54 @@ func newTestCmd() *cobra.Command {
 	return cmd
 }
 
-func logTest() {
+func logTest(){
 
+}
+
+func tidbTest() {
+	rownum := 1000000
+	batchSize := 5000
+	totalCost := time.Now()
+	//tidbUri := "root:@tcp(127.0.0.1:4000)/gangshen?charset=utf8"
+	mysqlUri := "root:letsg0@tcp(127.0.0.1:3308)/gangshen?charset=utf8"
+	conn, err := sql.Open("mysql", mysqlUri)
+	if err != nil{
+		panic(err)
+	}
+	if err = conn.Ping();err != nil{
+		fmt.Println("open database fail")
+	}
+	count := 0
+	for {
+		txnCost := time.Now()
+		//开启事务
+		tx, err := conn.Begin()
+		if err != nil {
+			fmt.Println("tx fail")
+		}
+		//准备sql语句
+		stmt, err := tx.Prepare("INSERT INTO tid (`id`) VALUES (?)")
+		if err != nil {
+			fmt.Println("Prepare fail")
+		}
+		for i := 0; i < batchSize; i++ {
+
+			//将参数传递到sql语句中并且执行
+			_, err := stmt.Exec(count+i)
+			if err != nil {
+				fmt.Println("Exec fail")
+			}
+		}
+
+		//将事务提交
+		tx.Commit()
+		count += batchSize
+		txnTime := time.Since(txnCost)
+		fmt.Printf("Total Inserted %d rows, Insert %d rows Cost %s\n",count,batchSize,txnTime)
+		if count >= rownum{
+			break
+		}
+	}
+	totalTime := time.Since(totalCost)
+	fmt.Printf("Total cost time:%s\n",totalTime)
 }
